ece5807ebf077cb0d8606562d6acfd3574b533d2
Jon Grythe Stødle <jon@indicosys.com>
2019-03-29 10:06:53 +0100

    Tweak Fixture to not create new sample files all the time

--- tests/TusDotNetClientTests/Fixture.cs
+++ tests/TusDotNetClientTests/Fixture.cs
@@ -10,11 +10,35 @@ namespace TusDotNetClientTests
     {
         private readonly Process _tusProcess;
 
-        public static DirectoryInfo DataDirectory;
+        public static readonly DirectoryInfo DataDirectory;
+
+        static Fixture()
+        {
+            DataDirectory = Directory.CreateDirectory("data");
+
+            var smallTextFile = new FileInfo(Path.Combine(DataDirectory.FullName, "small_text_file.txt"));
+            File.WriteAllText(smallTextFile.FullName, Guid.NewGuid().ToString());
+
+            var largeSampleFile = new FileInfo(Path.Combine(DataDirectory.FullName, "large_sample_file.bin"));
+            using (var fileStream = new FileStream(largeSampleFile.FullName, FileMode.Create, FileAccess.Write))
+            {
+                var bytes = new byte[1024 * 1024];
+                foreach (var _ in Enumerable.Range(0, 50))
+                {
+                    new Random().NextBytes(bytes);
+                    fileStream.Write(bytes, 0, bytes.Length);
+                }
+            }
+
+            TestFiles = new[]
+            {
+                new[] {smallTextFile},
+                new[] {largeSampleFile},
+            };
+        }
 
         public Fixture()
         {
-            DataDirectory = Directory.CreateDirectory("data");
             _tusProcess = Process.Start(new DirectoryInfo(Directory.GetCurrentDirectory())
                                             .Parent?
                                             .Parent?
@@ -26,22 +50,7 @@ namespace TusDotNetClientTests
                                             "tusd executable must be present in test project directory"));
         }
 
-        public static IEnumerable<object[]> GetTestFiles()
-        {
-            var smallTextFile = new FileInfo(Path.Combine(DataDirectory.FullName, "small_text_file.txt"));
-            File.WriteAllText(smallTextFile.FullName, Guid.NewGuid().ToString());
-            yield return new[] {smallTextFile};
-
-            var largeSampleFile = new FileInfo(Path.Combine(DataDirectory.FullName, "large_sample_file.bin"));
-            using (var fileStream = new FileStream(largeSampleFile.FullName, FileMode.Create, FileAccess.Write))
-            {
-                var bytes = new byte[50 * 1024 * 1024];
-                new Random().NextBytes(bytes);
-                fileStream.Write(bytes, 0, bytes.Length);
-            }
-
-            yield return new[] {largeSampleFile};
-        }
+        public static IEnumerable<object[]> TestFiles { get; }
 
         public void Dispose()
         {
